36607a2c925cb8f8a6e76c657d16a14e18aa6a5e
r.kamgo@oskar-kinderland.de <r.kamgo@oskar-kinderland.de>
2023-03-30 15:12:31 +0200

    update add update monolog config

--- project/src/Controller/StartController.php
+++ project/src/Controller/StartController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class StartController extends AbstractController
 {
+    public function __construct(
+        private LoggerInterface $logger
+    )
+    {
+        
+    }
     #[Route('/', name: 'app_start')]
     public function index(): Response
     {
+        $this->logger->info("Start logger");
         return $this->render('start/index.html.twig', [
             'controller_name' => 'StartController',
         ]);
